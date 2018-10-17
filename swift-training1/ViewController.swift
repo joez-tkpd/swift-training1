41b6def120c5cae5ab99d627743340afe83bc4cf
Nakama <nakama@202295s-imac.local>
2018-10-17 09:50:09 +0700

    Initial Commit

---
+++ swift-training1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swift-training1
+//
+//  Created by Nakama on 17/10/18.
+//  Copyright © 2018 Nakama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
