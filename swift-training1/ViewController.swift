0c73bda35857349b1bd1d0f5efe572c145cbcbbc
Nakama <nakama@202295s-iMac.local>
2018-10-19 09:58:29 +0700

    day2: view controller

--- swift-training1/ViewController.swift
+++ swift-training1/ViewController.swift
@@ -9,12 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var btnLogin: UIButton!
+    @IBOutlet weak var lblTitle: UILabel!
+    @IBOutlet weak var txtUsername: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    
         // Do any additional setup after loading the view, typically from a nib.
+        
+//        let viewTest = UIView(frame: CGRect(x: 100, y: 0, width: 100, height: 100))
+//        viewTest.backgroundColor=#colorLiteral(red: 0, green: 0.4784313725, blue: 1, alpha: 1)
+//        self.view.addSubview(viewTest)
+        
     }
-
+    
+    @IBAction func onLoginClicked(_ sender: Any) {
+        if !(txtUsername.text?.isEmpty ?? true ){
+            self.performSegue(withIdentifier: "toHome", sender: nil)
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
---
+++ swift-training1/jx.swift
@@ -0,0 +1,12 @@
+//
+//  asdasd.swift
+//  swift-training1
+//
+//  Created by Nakama on 17/10/18.
+//  Copyright © 2018 Nakama. All rights reserved.
+//
+
+import Foundation
+
+
+//9ab5bd5dd4397c463bf8fa48a370a0cc3da9eb87
